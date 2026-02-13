ccbfddca8fa2a169541f8dcf953be76ef3b089e3
lkadar2015 <138489076+lkadar2015@users.noreply.github.com>
2026-02-13 09:56:24 +0100

    Added restart flag to stats

--- frigate/types.py
+++ frigate/types.py
@@ -14,6 +14,7 @@ class StatsTrackingTypes(TypedDict):
     latest_frigate_version: str
     last_updated: int
     processes: dict[str, int]
+    restart: boolean
 
 
 class ModelStatusTypesEnum(str, Enum):
