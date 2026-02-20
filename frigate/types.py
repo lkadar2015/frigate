d4d03c1d1d7f3f50c19de4dc09675f4d63351a29
lkadar2015 <138489076+lkadar2015@users.noreply.github.com>
2026-02-20 09:35:17 +0100

    Revert changes

--- frigate/types.py
+++ frigate/types.py
@@ -14,7 +14,6 @@ class StatsTrackingTypes(TypedDict):
     latest_frigate_version: str
     last_updated: int
     processes: dict[str, int]
-    restart: bool
 
 
 class ModelStatusTypesEnum(str, Enum):
