e3e96234030bedc6d0d8fd089644ceb9b5563871
lkadar2015 <138489076+lkadar2015@users.noreply.github.com>
2026-05-12 11:24:38 +0200

    Update util.py

--- frigate/stats/util.py
+++ frigate/stats/util.py
@@ -477,7 +477,7 @@ def stats_snapshot(
             "latest_version": stats_tracking["latest_frigate_version"],
             "storage": {},
             "last_updated": int(time.time()),
-            "shall_restart": int(shall_restart),
+            "shall_restart": shall_restart,
         }
     else:
         stats["service"] = {
@@ -486,7 +486,7 @@ def stats_snapshot(
             "latest_version": stats_tracking["latest_frigate_version"],
             "storage": {},
             "last_updated": int(time.time()),
-            "shall_restart": int(shall_restart),
+            "shall_restart": shall_restart,
         }
 
     for path in [RECORD_DIR, CLIPS_DIR, CACHE_DIR]:
